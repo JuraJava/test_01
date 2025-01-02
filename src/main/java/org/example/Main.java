4e03dfc73e572f4bfdd38b9bc3a769b2e1c5010f
Jura Danilov <jura.danilov.2024@gmail.com>
2025-01-02 21:11:18 +0300

    Initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,19 +11,29 @@ public class Main {
     public static void main(String[] args) {
         Class<Driver> driverClass = Driver.class;
 
-        String str = "CREATE TABLE IF NOT EXISTS cars (" +
-                     "id INT PRIMARY KEY AUTO_INCREMENT," +
-                     "name VARCHAR(255)," +
-                     "speed DOUBLE )";
+        String str1 = "CREATE TABLE IF NOT EXISTS cars (" +
+                "id INT PRIMARY KEY," +
+                "name VARCHAR(255)," +
+                "speed DOUBLE )";
         String str2 = "DROP TABLE IF EXISTS cars;";
-
+        String str3 = "INSERT INTO cars VALUES " +
+                "(4,'Mazda', 150.0)," +
+                "(5, 'BMW', 225.0)," +
+                "(6, 'AUDI', 200.0);";
+        String str4 = "UPDATE cars SET speed=333.0 " +
+                "WHERE id IN (4, 5, 6)";
         try {
             Connection connection = ConnectionManager.open();
             Statement statement = connection.createStatement();
-//            statement.executeUpdate(str);
-            statement.executeUpdate(str2);
-
-            System.out.println(connection.getTransactionIsolation());
+//            statement.executeUpdate(str1);
+//            statement.executeUpdate(str2);
+//            int temp1 = statement.executeUpdate(str3);
+//            int temp1 = statement.executeUpdate(str4);
+            boolean temp2 = statement.execute(str4);
+//            System.out.println(connection.getTransactionIsolation());
+//            System.out.println("Значение переменной temp " + temp1);
+            System.out.println("Значение переменной temp " + temp2);
+            System.out.println("Количество вставленных записей = " + statement.getUpdateCount());
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
