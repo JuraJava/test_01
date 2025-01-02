6e8a59c49b6f603694297225fbbe6828adc70de5
Jura Danilov <jura.danilov.2024@gmail.com>
2025-01-02 21:49:26 +0300

    Initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 import com.mysql.cj.jdbc.Driver;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -22,18 +23,30 @@ public class Main {
                 "(6, 'AUDI', 200.0);";
         String str4 = "UPDATE cars SET speed=333.0 " +
                 "WHERE id IN (4, 5, 6)";
+        String str5 = "SELECT id, speed, name FROM cars WHERE name='BMW'";
         try {
             Connection connection = ConnectionManager.open();
             Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery(str5);
+
+            while (resultSet.next()) {
+                System.out.print(resultSet.getString(3) + " - ");
+                System.out.println(resultSet.getDouble("speed"));
+            }
+// Этот цикл используется для того чтобы выбираемые из таблицы данные отображались понятно
+
+            System.out.println(resultSet);
+
 //            statement.executeUpdate(str1);
 //            statement.executeUpdate(str2);
 //            int temp1 = statement.executeUpdate(str3);
 //            int temp1 = statement.executeUpdate(str4);
-            boolean temp2 = statement.execute(str4);
+//            boolean temp2 = statement.execute(str4);
 //            System.out.println(connection.getTransactionIsolation());
 //            System.out.println("Значение переменной temp " + temp1);
-            System.out.println("Значение переменной temp " + temp2);
-            System.out.println("Количество вставленных записей = " + statement.getUpdateCount());
+//            System.out.println("Значение переменной temp " + temp2);
+//            System.out.println("Количество вставленных записей = " + statement.getUpdateCount());
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
