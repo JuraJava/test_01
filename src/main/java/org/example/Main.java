b970bbaa3ca019ad440abc305134e3aef1669efc
Jura Danilov <jura.danilov.2024@gmail.com>
2025-01-02 22:52:50 +0300

    Initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,10 +6,15 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        System.out.println("Введите id автомобиля:");
+        String carId = new Scanner(System.in).nextLine();
+
         Class<Driver> driverClass = Driver.class;
 
         String str1 = "CREATE TABLE IF NOT EXISTS cars (" +
@@ -23,20 +28,28 @@ public class Main {
                 "(6, 'AUDI', 200.0);";
         String str4 = "UPDATE cars SET speed=333.0 " +
                 "WHERE id IN (4, 5, 6)";
-        String str5 = "SELECT id, speed, name FROM cars WHERE name='BMW'";
+//        String str5 = "SELECT id, speed, name FROM cars WHERE name='BMW'";
+        String str5 = "SELECT id, name, speed FROM cars WHERE id = " + carId;
+        String str6 = "INSERT INTO cars (name, speed) VALUES " +
+                "('SUBARU', 777.0);";
         try {
             Connection connection = ConnectionManager.open();
             Statement statement = connection.createStatement();
 
-            ResultSet resultSet = statement.executeQuery(str5);
+//            ResultSet resultSet = statement.executeQuery(str5);
+            statement.execute(str5);
+            ResultSet resultSet = statement.getResultSet();
 
             while (resultSet.next()) {
-                System.out.print(resultSet.getString(3) + " - ");
+//                System.out.print(resultSet.getString(3) + " - ");
+//                System.out.println(resultSet.getDouble("speed"));
+                System.out.print(resultSet.getInt("id") + " ");
+                System.out.print(resultSet.getString("name") + " ");
                 System.out.println(resultSet.getDouble("speed"));
             }
 // Этот цикл используется для того чтобы выбираемые из таблицы данные отображались понятно
 
-            System.out.println(resultSet);
+//            System.out.println(resultSet);
 
 //            statement.executeUpdate(str1);
 //            statement.executeUpdate(str2);
