56af8c0ba368f62ca04f4a10fe6cb351956f2879
Jura Danilov <jura.danilov.2024@gmail.com>
2025-01-03 19:48:56 +0300

    Initial commit

--- src/dao/CarDao.java
+++ src/dao/CarDao.java
@@ -12,10 +12,22 @@ public class CarDao {
 
     private static final String DELETE_SQL = "DELETE FROM cars WHERE id = ?;";
 
+    private static final String ADD_NEW_CAR_SQL = "INSERT INTO cars(id, name, speed)" +
+                                                  "VALUE (?, ?, ?);";
+
     private CarDao() {
     }
     // Конструктор созаём приватным чтобы никто не смог
     // создать экземпляр класса
+
+    public static void addNewCar(int id, String name, double speed) throws SQLException {
+        Connection connection = ConnectionManager.get();
+        PreparedStatement preparedStatement = connection.prepareStatement(ADD_NEW_CAR_SQL);
+        preparedStatement.setInt(1, id);
+        preparedStatement.setString(2, name);
+        preparedStatement.setDouble(3, speed);
+        preparedStatement.executeUpdate();
+    }
 
     public static void delete(int id) throws SQLException {
         Connection connection = ConnectionManager.get();
