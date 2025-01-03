514a7c851d0f4be80f1916dd57a8bd64d560a138
Jura Danilov <jura.danilov.2024@gmail.com>
2025-01-03 11:04:59 +0300

    Initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,8 +14,8 @@ public class Main {
         int carId = Integer.parseInt(new Scanner(System.in).nextLine());
         // Эту строку вместо закомментированной сверху мы используем чтобы
         // избежать SQL инъекций, когда там будут писать OR (строку) - программа не сработает
-        System.out.println("Введите марку автомобиля:");
-        String nameCar = (new Scanner(System.in).nextLine()).trim();
+//        System.out.println("Введите марку автомобиля:");
+//        String nameCar = (new Scanner(System.in).nextLine()).trim();
 
         Class<Driver> driverClass = Driver.class;
 
@@ -32,7 +32,8 @@ public class Main {
                 "WHERE id IN (4, 5, 6)";
 //        String str5 = "SELECT id, speed, name FROM cars WHERE name='BMW'";
 //        String str5 = "SELECT id, name, speed FROM cars WHERE id = " + carId;
-        String str5 = "SELECT id, name, speed FROM cars WHERE id = ? OR name = ?";
+//        String str5 = "SELECT id, name, speed FROM cars WHERE id = ? OR name = ?";
+        String str5 = "SELECT id, name, speed FROM cars WHERE id >= ?";
         String str6 = "INSERT INTO cars (name, speed) VALUES " +
                 "('SUBARU', 777.0);";
         try {
@@ -41,7 +42,16 @@ public class Main {
             PreparedStatement preparedStatement = connection.prepareStatement(str5);
 
             preparedStatement.setInt(1, carId);
-            preparedStatement.setString(2, nameCar);
+
+//            preparedStatement.setMaxRows(3);
+//  Этот метод нужен чтобы при вводе, например id = 1 мы могли получить столько записей,
+//  сколько указано в параметре метода setMaxRows(3), начиная с того id который мы ввели
+            preparedStatement.setFetchSize(3);
+// Этот метод используется для того чтобы данные нам выбирались порциями по столько
+// записей, сколько указано в параметре метода setFetchSize(3).
+// Отображаться будут все записи, но выбираться под капотом они будут по столько
+
+//            preparedStatement.setString(2, nameCar);
 //            ResultSet resultSet = statement.executeQuery(str5);
 //            statement.execute(str5);
             preparedStatement.execute();
